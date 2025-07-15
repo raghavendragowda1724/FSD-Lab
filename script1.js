565251ee30f21481326b1df89bb48c4bf43b2128
raghavendragowda1724 <raghavendragowda1724@gmail.com>
2025-07-15 14:00:15 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,20 @@
+function submit(){
+
+    var fullname=document.getElementById('fullname')
+    console.log(fullname.value)
+
+    var phonenumber=document.getElementById('phonenumber')
+    console.log(phonenumber.value)
+
+    var emailaddress=document.getElementById('emailaddress')
+    console.log(emailaddress.value)
+
+    var Foodpreferance=document.getElementById('foodpreference')
+    console.log(Foodpreferance.value)
+
+    var Tshirt=document.getElementById('Tshirt')
+    console.log(Tshirt.value)
+
+    var Gender=document.getElementById('gender')
+    console.log(Gender.value)
+}
